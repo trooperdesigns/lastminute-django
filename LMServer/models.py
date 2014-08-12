72c90af0965a33ac18f880578952d5f95517ed83
Juuhhhstin <justin.kj.chang@gmail.com>
2014-08-11 21:08:26 -0400

    initial commit for lmuser branch

--- LMServer/models.py
+++ LMServer/models.py
@@ -16,15 +16,19 @@ class Event(models.Model):
 		db_table = 'events'
 
 class LMUser(models.Model):
-	user = models.OneToOneField(User)
+	user = models.OneToOneField(User, primary_key=True)
 
 	username = models.CharField(max_length=25)
 
 	fbUser = models.CharField(max_length=100)
-	twitterUser = models.CharField(max_length=100)
-	googleUser = models.CharField(max_length=100)
+	#twitterUser = models.CharField(max_length=100)
+	#googleUser = models.CharField(max_length=100)
 
 	created = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
 		db_table = 'lmusers'
+
+	def __unicode__(self, user, fbUser):
+		self.user = user
+		self.fbUser = fbUser
--- LMServer/serializers.py
+++ LMServer/serializers.py
@@ -1,16 +1,33 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from LMServer.models import Event, LMUser
+from django.db import models
+import time, datetime
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    fbUser = serializers.SerializerMethodField('createLMUser')
+ 
+    ts = time.time()
+    date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    created = date
+
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('url', 'username', 'email', 'groups', 'fbUser')
+
+    def createLMUser(self, obj):
+        #print "obj" + obj.username
+        lm1 = LMUser(user=obj, fbUser="12345", created=UserSerializer.created)
+        lm1.save()
+        return lm1.fbUser
+
+
 
 class LMUserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = LMUser
-        fields = ('url', 'username', 'fbUser', 'twitterUser', 'googleUser', 'created')
+        fields = ('fbUser', 'twitterUser', 'googleUser', 'created')
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
